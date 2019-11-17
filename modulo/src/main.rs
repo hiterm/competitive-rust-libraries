c3c2d33cab16111d64936544f8a841944caffe19
htlsne <htlsne23@gmail.com>
2019-11-17 14:01:35 +0900

    Rewrite inv and binom_coef

--- modulo/src/main.rs
+++ modulo/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     println!("{:?}", Modulo(5) * Modulo(5));
     println!("{:?}", Modulo(3).pow(2));
     println!("{:?}", Modulo(3).pow(1000000000000000));
+    println!("{:?}", Modulo(2).inv());
+    println!("{:?}", Modulo(3).inv());
 }
 
 fn positive_rem(a: isize, b: usize) -> isize {
@@ -18,6 +20,18 @@ fn positive_rem(a: isize, b: usize) -> isize {
     value
 }
 
+/// Return (x, y) s.t. ax + by = d where d = gcd(a, b)
+fn ext_gcd(a: usize, b: usize) -> (isize, isize) {
+    if b == 0{
+        return (1, 0);
+    }
+
+    let q = (a / b) as isize;
+    let r = a % b;
+    let (x1, y1) = ext_gcd(b, r);
+    (y1, x1 - q * y1)
+}
+
 #[derive(Debug, Copy, Clone)]
 struct Modulo(isize);
 
@@ -26,7 +40,7 @@ const MODULO: isize = 11;
 impl Modulo {
 
     fn new(n: isize) -> Modulo {
-        Modulo(n % MODULO)
+        Modulo(positive_rem(n, MODULO as usize))
     }
 
     fn pow(self, p: usize) -> Modulo {
@@ -45,17 +59,17 @@ impl Modulo {
         }
     }
 
-    // when p is prime
+    // when MODULO is prime
     fn inv(self) -> Modulo {
-        self.pow((MODULO - 2) as usize)
+        let (x, _) = ext_gcd(self.0 as usize, MODULO as usize);
+        Modulo::new(x)
     }
 
-    // when p is prime
+    // when MODULO is prime
     fn binom_coef(n: isize, k: isize) -> Modulo {
         let mut ret = Modulo::new(1);
         for i in 1..(k + 1) {
             ret = ret * Modulo::new(n - i + 1) * Modulo::new(i).inv();
-            // debugln!("{:?}", ret);
         }
         ret
     }
@@ -136,6 +150,17 @@ mod tests {
     fn inv() {
         assert_eq!(Modulo(1), Modulo(2) * Modulo(2).inv());
         assert_eq!(Modulo(1), Modulo(3) * Modulo(3).inv());
+    }
+
+    #[test]
+    fn ext_gcd_test() {
+        let a = 12isize; let b = 18isize;
+        let (x, y) = ext_gcd(a as usize, b as usize);
+        assert_eq!(6, a * x + b * y);
+
+        let a = 4isize; let b = 12isize;
+        let (x, y) = ext_gcd(a as usize, b as usize);
+        assert_eq!(4, a * x + b * y);
     }
 
     #[test]
