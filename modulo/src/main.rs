da02261be39407a983311679445820835ec002ca
htlsne <htlsne23@gmail.com>
2019-11-17 12:46:13 +0900

    Add and modify tests

--- modulo/src/main.rs
+++ modulo/src/main.rs
@@ -21,7 +21,7 @@ fn positive_rem(a: isize, b: usize) -> isize {
 #[derive(Debug, Copy, Clone)]
 struct Modulo(isize);
 
-const MODULO: isize = 10;
+const MODULO: isize = 11;
 
 impl Modulo {
 
@@ -45,11 +45,13 @@ impl Modulo {
         }
     }
 
+    // when p is prime
     fn inv(self) -> Modulo {
         self.pow((MODULO - 2) as usize)
     }
 
-    fn binom(n: isize, k: isize) -> Modulo {
+    // when p is prime
+    fn binom_coef(n: isize, k: isize) -> Modulo {
         let mut ret = Modulo::new(1);
         for i in 1..(k + 1) {
             ret = ret * Modulo::new(n - i + 1) * Modulo::new(i).inv();
@@ -102,12 +104,12 @@ mod tests {
 
     #[test]
     fn add1() {
-        assert_eq!(Modulo(1), Modulo(5) + Modulo(6));
+        assert_eq!(Modulo(1), Modulo(6) + Modulo(6));
     }
 
     #[test]
     fn add2() {
-        assert_eq!(Modulo(0), Modulo(4) + Modulo(6));
+        assert_eq!(Modulo(0), Modulo(5) + Modulo(6));
     }
 
     #[test]
@@ -117,16 +119,27 @@ mod tests {
 
     #[test]
     fn sub2() {
-        assert_eq!(Modulo(9), Modulo(1) - Modulo(2));
+        assert_eq!(Modulo(10), Modulo(1) - Modulo(2));
     }
 
     #[test]
     fn mul() {
-        assert_eq!(Modulo(5), Modulo(5) * Modulo(5));
+        assert_eq!(Modulo(3), Modulo(5) * Modulo(5));
     }
 
     #[test]
     fn pow() {
-        assert_eq!(Modulo(9), Modulo(7).pow(2));
+        assert_eq!(Modulo(5), Modulo(7).pow(2));
+    }
+
+    #[test]
+    fn inv() {
+        assert_eq!(Modulo(1), Modulo(2) * Modulo(2).inv());
+        assert_eq!(Modulo(1), Modulo(3) * Modulo(3).inv());
+    }
+
+    #[test]
+    fn binom_coef() {
+        assert_eq!(Modulo(6), Modulo::binom_coef(4, 2));
     }
 }
