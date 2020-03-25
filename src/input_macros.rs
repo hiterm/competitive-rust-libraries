76109f1a3f5d0149efef66f9513b83251e43b388
htlsne <htlsne23@gmail.com>
2020-03-25 22:16:21 +0900

    Add input macros

---
+++ src/input_macros.rs
@@ -0,0 +1,29 @@
+#[allow(unused_macros)]
+macro_rules! getl {
+    ( $( $t:ty ),* ) => {
+        {
+            let mut s = String::new();
+            std::io::stdin().read_line(&mut s).unwrap();
+            let mut ws = s.split_whitespace();
+            ($(ws.next().unwrap().parse::<$t>().unwrap()),*)
+        }
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! getl_vec {
+    ( $t:ty ) => {
+        {
+            let mut s = String::new();
+            std::io::stdin().read_line(&mut s).unwrap();
+            s.split_whitespace().map(|x| x.parse().unwrap()).collect::<Vec<$t>>()
+        }
+    };
+}
+
+fn main() {
+    let (a, b) = getl(usize, usize);
+    let v = getl_vec(usize);
+    println!("{} {}", a, b);
+    println!("{:?}", v);
+}
