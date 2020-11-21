1180d920a59e5ac1d30e0669430ac1af3039c347
htlsne <htlsne23@gmail.com>
2020-11-21 19:03:37 +0900

    Add cumulative_sum test

--- src/cumulative_sum.rs
+++ src/cumulative_sum.rs
@@ -1,6 +1,6 @@
 fn cumulative_sum<T>(v: &Vec<T>) -> Vec<T>
 where
-    T: Clone + std::ops::Add<Output = T> + From<u32>,
+    T: Clone + std::ops::Add<Output = T> + From<u8>,
 {
     let zero: T = 0.into();
     let mut sum = vec![zero.clone()];
@@ -11,4 +11,22 @@ where
     }
 
     sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+#[test]
+    fn cumulative_sum_i64_test() {
+        let v: Vec<i64> = vec![1, 2, 3];
+        let csum = cumulative_sum(&v);
+        assert_eq!(vec![0, 1, 3, 6], csum);
+    }
+
+    fn cumulative_sum_usize_test() {
+        let v: Vec<usize> = vec![1, 2, 3];
+        let csum = cumulative_sum(&v);
+        assert_eq!(vec![0, 1, 3, 6], csum);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod btreeset_util;
 pub mod multi_vec;
 pub mod chmax_chmin;
 pub mod binary_search;
+pub mod cumulative_sum;
