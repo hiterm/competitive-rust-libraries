8f4870081f017bfb6afd8a40e0cee5ebee768ed8
htlsne <htlsne23@gmail.com>
2020-11-21 18:49:33 +0900

    Write multi_vec test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod btreeset_util;
+pub mod multi_vec;
--- src/multi_vec.rs
+++ src/multi_vec.rs
@@ -3,3 +3,14 @@ macro_rules! multi_vec {
     ( $elem:expr; $num:expr ) => (vec![$elem; $num]);
     ( $elem:expr; $num:expr, $($rest:expr),* ) => (vec![multi_vec![$elem; $($rest),*]; $num]);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn multi_vec_test() {
+        let v = multi_vec![0; 2, 2];
+        assert_eq!(vec![vec![0, 0], vec![0, 0]], v);
+    }
+}
