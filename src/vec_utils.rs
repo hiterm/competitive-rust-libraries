5d318a842d1598eee68880e6f19f0bf3020fe7cb
htlsne <htlsne23@gmail.com>
2020-11-28 16:52:06 +0900

    Fix

--- src/vec_utils.rs
+++ src/vec_utils.rs
@@ -1,8 +1,8 @@
 pub fn eprint_2dim_vec<T: ToString>(v: &[Vec<T>]) {
-    println!("[");
+    eprintln!("[");
     for row in v {
         let s = row.iter().map(|x| x.to_string()).collect::<Vec<_>>().join(", ");
-        println!("    [{}],", s);
+        eprintln!("    [{}],", s);
     }
-    println!("]");
+    eprintln!("]");
 }
