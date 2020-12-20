b9a2ae821e6c15092f50500daf92254a65ee4386
htlsne <htlsne23@gmail.com>
2020-12-20 18:51:34 +0900

    Add ext_gcd and mod_inv

--- src/number_theory.rs
+++ src/number_theory.rs
@@ -1,5 +1,5 @@
 #![allow(unused)]
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
 pub fn gcd(a: usize, b: usize) -> usize {
     let c = a % b;
@@ -14,6 +14,22 @@ pub fn lcm(a: usize, b: usize) -> usize {
     a / gcd(a, b) * b
 }
 
+pub fn mod_inv(x: i64, m: i64) -> i64 {
+    let (x, _) = ext_gcd(x, m);
+    ((x % m) + m) % m
+}
+
+pub fn ext_gcd(a: i64, b: i64) -> (i64, i64) {
+    if b == 0 {
+        return (1, 0);
+    }
+
+    let q = a / b;
+    let r = a % b;
+    let (x1, y1) = ext_gcd(b, r);
+    (y1, x1 - q * y1)
+}
+
 pub fn prime_sieve(n: usize) -> Vec<u64> {
     let mut table: Vec<u64> = vec![0; n + 1];
     let mut primes: Vec<u64> = Vec::new();
@@ -23,7 +39,7 @@ pub fn prime_sieve(n: usize) -> Vec<u64> {
             primes.push(i as u64);
             for j in 2..n {
                 if i * j > n {
-                    break
+                    break;
                 }
                 table[i * j] = 1
             }
@@ -53,7 +69,9 @@ pub fn prime_factor(n: u64) -> HashMap<u64, u64> {
 
 pub fn divisors_vec(n: u64) -> Vec<u64> {
     prime_factor(n).iter().fold(vec![1], |acc, (&p, &pow)| {
-        (0..=pow).flat_map(|i| acc.iter().map(move |a| a * p.pow(i as u32))).collect()
+        (0..=pow)
+            .flat_map(|i| acc.iter().map(move |a| a * p.pow(i as u32)))
+            .collect()
     })
 }
 
@@ -62,7 +80,9 @@ pub fn divisors_set(n: u64) -> HashSet<u64> {
     set.insert(1);
 
     prime_factor(n).iter().fold(set, |acc, (&p, &pow)| {
-        (0..=pow).flat_map(|i| acc.iter().map(move |a| a * p.pow(i as u32))).collect()
+        (0..=pow)
+            .flat_map(|i| acc.iter().map(move |a| a * p.pow(i as u32)))
+            .collect()
     })
 }
 
@@ -134,7 +154,7 @@ impl Factorize {
 mod tests {
     use super::*;
     #[test]
-    fn binom_coef_test(){
+    fn binom_coef_test() {
         assert_eq!(6, binom_coef(4, 2));
         assert_eq!(3, binom_coef(3, 2));
         assert_eq!(20, binom_coef(6, 3));
@@ -142,5 +162,24 @@ mod tests {
         assert_eq!(10, binom_coef(10, 1));
         assert_eq!(1, binom_coef(10, 10));
         assert_eq!(0, binom_coef(2, 3));
+    }
+
+    #[test]
+    fn ext_gcd_test_5_7() {
+        let (a, b) = ext_gcd(5, 7);
+        let actual = a * 5 + b * 7;
+        assert_eq!(1, actual);
+    }
+
+    #[test]
+    fn ext_gcd_test_4_6() {
+        let (a, b) = ext_gcd(4, 6);
+        let actual = a * 4 + b * 6;
+        assert_eq!(2, actual);
+    }
+
+    #[test]
+    fn mod_inv_test() {
+        assert_eq!(1, (3 * mod_inv(3, 7)) % 7);
     }
 }
