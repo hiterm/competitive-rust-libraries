ef8dec60764fbb933599d1b534760b28dceaecff
htlsne <htlsne23@gmail.com>
2021-02-07 13:48:32 +0900

    Remove warning

--- src/decimal.rs
+++ src/decimal.rs
@@ -1,4 +1,4 @@
-fn parse_decimal(s: &str) -> (i64, i64) {
+pub fn parse_decimal(s: &str) -> (i64, i64) {
     if !s.contains(".") {
         let int_part: i64 = s.parse().unwrap();
         (int_part, 0)
