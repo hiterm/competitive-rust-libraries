a4a05a9c97a3b02bbd5813f812407fa5bd9ec29f
htlsne <htlsne23@gmail.com>
2021-02-12 18:17:50 +0900

    Update vec_utils

--- src/bin/vec_utils.rs
+++ src/bin/vec_utils.rs
@@ -2,5 +2,5 @@ use competitive_rust_libraries::vec_utils;
 
 fn main() {
     let v = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    vec_utils::eprint_2dim_vec(&v);
+    eprintln!("{}", vec_utils::string_2dim_vec(&v));
 }
--- src/vec_utils.rs
+++ src/vec_utils.rs
@@ -1,12 +1,15 @@
-pub fn eprint_2dim_vec<T: ToString>(v: &[Vec<T>]) {
-    eprintln!("[");
+pub fn string_2dim_vec<T: ToString>(v: &[Vec<T>]) -> String {
+    let mut ret = String::new();
+    ret.push_str("[\n");
     for row in v {
         let s = row
             .iter()
             .map(|x| x.to_string())
             .collect::<Vec<_>>()
             .join(", ");
-        eprintln!("    [{}],", s);
+        ret.push_str(&format!("    [{}],\n", s));
     }
-    eprintln!("]");
+    ret.push_str("]");
+
+    ret
 }
