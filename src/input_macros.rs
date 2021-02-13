fea309dce85ec19231fff34b259cb371855c34b4
htlsne <htlsne23@gmail.com>
2021-02-13 14:47:10 +0900

    receive BufRead

--- src/input_macros.rs
+++ src/input_macros.rs
@@ -1,33 +1,42 @@
 #[allow(unused_macros)]
+#[macro_export]
 macro_rules! getl {
-    ( $( $t:ty ),* ) => {
+    ( $r:expr; $( $t:tt ),* ) => {
         {
+            use std::io::BufRead;
             let mut s = String::new();
-            std::io::stdin().read_line(&mut s).unwrap();
+            $r.read_line(&mut s).unwrap();
             let s = s.trim_end();
             let mut ws = s.split_whitespace();
-            ($(ws.next().unwrap().parse::<$t>().unwrap()),*)
+            ( $( $crate::getl_parse_one!( ws, $t ) ),* )
         }
     };
 }
 
 #[allow(unused_macros)]
-macro_rules! getl_vec {
-    ( $t:ty ) => {
-        {
-            let mut s = String::new();
-            std::io::stdin().read_line(&mut s).unwrap();
-            let s = s.trim_end();
-            s.split_whitespace().map(|x| x.parse().unwrap()).collect::<Vec<$t>>()
-        }
+#[macro_export]
+macro_rules! getl_parse_one {
+    // ($it:expr, [$t:ty; $n:expr]) => {
+    //     let mut v = vec![];
+    //     for _ in 0..$n {
+    //         v.push($it.next().unwrap().parse::<$t>().unwrap());
+    //     }
+    //     v
+    // };
+    ($it:expr, $t:ty) => {
+        $it.next().unwrap().parse::<$t>().unwrap()
     };
 }
 
-#[allow(unused)]
-fn main() {
-    let (a, b) = getl!(usize, usize);
-    let v = getl_vec!(usize);
-    let s = getl!(String);
-    println!("{} {}", a, b);
-    println!("{:?}", v);
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! getl_vec {
+    ( $t:ty ) => {{
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        let s = s.trim_end();
+        s.split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<$t>>()
+    }};
 }
